e0f25c0819bc307cc2703438570d47f5ea95eff9
Erruuu <64939964+EruBot@users.noreply.github.com>
2022-04-09 03:05:32 +0700

    new stats

--- userbot/modules/stats.py
+++ userbot/modules/stats.py
@@ -1,27 +1,31 @@
-"""Count the Number of Dialogs you have in your Telegram Account
-Syntax: .stats"""
-import logging
 import time
-from telethon import events
+
 from telethon.events import NewMessage
 from telethon.tl.custom import Dialog
+from telethon.tl.functions.contacts import GetBlockedRequest
+from telethon.tl.functions.messages import GetAllStickersRequest
 from telethon.tl.types import Channel, Chat, User
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import bot, CMD_HELP
+
+from userbot import CMD_HELP
 from userbot.events import register
 
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
-)
-logger = logging.getLogger(__name__)
+
+def user_full_name(user):
+    nick = [user.first_name, user.last_name]
+    nick = [i for i in list(nick) if i]
+    return " ".join(nick)
 
 
-@register(outgoing=True, pattern=r"^.stats(?: |$)(.*)")
+def user_tag(user):
+    mentions = user_full_name(user) or "No Name"
+    return f"[{mentions}](tg://user?id={user.id})"
+
+
+@register(outgoing=True, pattern=r"^\.stats ?(.*)")
 async def stats(
     event: NewMessage.Event,
-) -> None:  # pylint: disable = R0912, R0914, R0915
-    """Command to get stats about the account"""
-    await event.edit("`Collecting stats, Wait Master`")
+) -> None:
+    stat = await event.edit("`Collecting stats...`")
     start_time = time.time()
     private_chats = 0
     bots = 0
@@ -36,129 +40,101 @@ async def stats(
     dialog: Dialog
     async for dialog in event.client.iter_dialogs():
         entity = dialog.entity
-
-        if isinstance(entity, Channel):
-            # participants_count = (await event.get_participants(dialog,
-            # limit=0)).total
-            if entity.broadcast:
-                broadcast_channels += 1
-                if entity.creator or entity.admin_rights:
-                    admin_in_broadcast_channels += 1
-                if entity.creator:
-                    creator_in_channels += 1
-
-            elif entity.megagroup:
-                groups += 1
-                # if participants_count > largest_group_member_count:
-                #     largest_group_member_count = participants_count
-                if entity.creator or entity.admin_rights:
-                    # if participants_count > largest_group_with_admin:
-                    #     largest_group_with_admin = participants_count
-                    admin_in_groups += 1
-                if entity.creator:
-                    creator_in_groups += 1
-
-        elif isinstance(entity, User):
-            private_chats += 1
-            if entity.bot:
-                bots += 1
-
-        elif isinstance(entity, Chat):
+        if isinstance(entity, Channel) and entity.broadcast:
+            broadcast_channels += 1
+            if entity.creator or entity.admin_rights:
+                admin_in_broadcast_channels += 1
+            if entity.creator:
+                creator_in_channels += 1
+        elif (
+            isinstance(entity, Channel)
+            and entity.megagroup
+            or not isinstance(entity, Channel)
+            and not isinstance(entity, User)
+            and isinstance(entity, Chat)
+        ):
             groups += 1
             if entity.creator or entity.admin_rights:
                 admin_in_groups += 1
             if entity.creator:
                 creator_in_groups += 1
-
+        elif not isinstance(entity, Channel) and isinstance(entity, User):
+            private_chats += 1
+            if entity.bot:
+                bots += 1
         unread_mentions += dialog.unread_mentions_count
         unread += dialog.unread_count
     stop_time = time.time() - start_time
-
-    full_name = inline_mention(await event.client.get_me())
-    response = f"🔸 **Stats for {full_name}** \n\n"
+    try:
+        ct = (await event.client(GetBlockedRequest(1, 0))).count
+    except AttributeError:
+        ct = 0
+    try:
+        sp = await event.client(GetAllStickersRequest(0))
+        sp_count = len(sp.sets)
+    except BaseException:
+        sp_count = 0
+    full_name = user_tag(await event.client.get_me())
+    response = f"📊 **Stats for {full_name}** \n\n"
     response += f"**Private Chats:** {private_chats} \n"
-    response += f"   • `Users: {private_chats - bots}` \n"
-    response += f"   • `Bots: {bots}` \n"
+    response += f"**  •• **`Users: {private_chats - bots}` \n"
+    response += f"**  •• **`Bots: {bots}` \n"
     response += f"**Groups:** {groups} \n"
     response += f"**Channels:** {broadcast_channels} \n"
     response += f"**Admin in Groups:** {admin_in_groups} \n"
-    response += f"   • `Creator: {creator_in_groups}` \n"
-    response += f"   • `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
+    response += f"**  •• **`Creator: {creator_in_groups}` \n"
+    response += f"**  •• **`Admin Rights: {admin_in_groups - creator_in_groups}` \n"
     response += f"**Admin in Channels:** {admin_in_broadcast_channels} \n"
-    response += f"   • `Creator: {creator_in_channels}` \n"
-    response += (
-        f"   • `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
-    )
+    response += f"**  •• **`Creator: {creator_in_channels}` \n"
+    response += f"**  •• **`Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
     response += f"**Unread:** {unread} \n"
-    response += f"**Unread Mentions:** {unread_mentions} \n\n"
-    response += f"__It Took:__ {stop_time:.02f}s \n"
-
-    await event.edit(response)
-
-
-def make_mention(user):
-    if user.username:
-        return f"@{user.username}"
-    else:
-        return inline_mention(user)
+    response += f"**Unread Mentions:** {unread_mentions} \n"
+    response += f"**Blocked Users:** {ct}\n"
+    response += f"**Total Stickers Pack Installed :** `{sp_count}`\n\n"
+    response += f"⏱ **__It Took:__** {stop_time:.02f}s \n"
+    await stat.edit(response)
 
 
-def inline_mention(user):
-    full_name = user_full_name(user) or "No Name"
-    return f"[{full_name}](tg://user?id={user.id})"
-
-
-def user_full_name(user):
-    names = [user.first_name, user.last_name]
-    names = [i for i in list(names) if i]
-    full_name = " ".join(names)
-    return full_name
-
-
-@register(outgoing=True, pattern=r"^\.ustat")
+@register(outgoing=True, pattern=r"^\.ustats ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
-    if not event.reply_to_msg_id:
-        return await event.edit("```Balas di Pesan Goblok!!.```")
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
     reply_message = await event.get_reply_message()
-    if not reply_message.text:
-        return await event.edit("```Balas di Pesan Goblok!!```")
+    if not input_str and not reply_message:
+        await event.edit(
+            "`Reply to user messages or provide userid/username`",
+        )
+    if input_str:
+        try:
+            uid = int(input_str)
+        except ValueError:
+            try:
+                u = await event.client.get_entity(input_str)
+            except ValueError:
+                await event.edit("`Provide userid or username to view group history`")
+            uid = u.id
+    else:
+        uid = reply_message.sender_id
     chat = "@tgscanrobot"
-    await event.edit("Checking Group User....")
+    sevent = await event.edit("`Processing...`")
     async with event.client.conversation(chat) as conv:
         try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1557162396)
-            )
-            msg = await event.client.forward_messages(chat, reply_message)
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("unblock me @tgscanrobot to work")
-            return
-        if response.text.startswith("I understand only text"):
-            await event.edit("Sorry i cant't check group this user **BURIK!!**")
-        else:
-            if response.text.startswith("Information"):
-                response = conv.wait_event(
-                    events.NewMessage(incoming=True, from_users=1557162396)
-                )
-                response = await response
-                await event.delete()
-                await event.client.send_message(
-                    event.chat_id, response.message, reply_to=reply_message.id
-                )
-                await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
-            else:
-                await event.edit("try again")
-        await bot.send_read_acknowledge(conv.chat_id)
+            msg = await conv.send_message(f"{uid}")
+        except Exception:
+            await sevent.edit("**Unblock @tgscanrobot and try again**")
+        response = await conv.get_response()
+        await event.client.send_read_acknowledge(conv.chat_id)
+        await sevent.edit(response.text)
+        """Cleanup after completed"""
+        await event.client.delete_messages(conv.chat_id, [msg.id, response.id])
 
 
 CMD_HELP.update(
     {
-        "stats": ">`.stats`"
-        "\nUsage: To check user stats"
-        "\n\n>`.ustat`"
-        "\nUsage: to check user group join"
+        "user_stats": ">`.stats`"
+        "\nUsage: To check user statistics"
+        "\n\n>`.ustats`"
+        "\nUsage: To check which group the person has joined"
     }
 )
